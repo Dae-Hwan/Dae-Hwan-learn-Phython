a0915abca4d8a7ea69f3f862ef9f0a35801cbab7
Dae-Hwan <gigymi2005@naver.com>
2020-11-26 15:32:40 +0900

    Create clock program(2)

--- pythonProject/venv/class.py
+++ pythonProject/venv/class.py
@@ -1,61 +1,69 @@
-class Clock:
-    # 1시 30분 48초인 시계 인스턴스 생성
-    def __init__(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+class Counter:
+    """
+    시계 클래스의 시,분,초를 각각 나타내는데 사용될 카운터 클래스
+    """
 
-    # 13초를 늘린다, 그리고 변환한다.
+    def __init__(self, limit):
+        """
+        인스턴스 변수 limit(최댓값), value(현재까지 카운트한 값)을 설정한다.
+        인스턴스를 생성할 때 인스턴스 변수 limit만 파라미터로 받고, value는 초깃값 0으로 설정한다.
+        """
+        self.limit = limit
+        self.value = 0
+
+    def set(self, new_value):
+        """
+        파라미터가 0 이상, 최댓값 미만이면 value에 설정한다.
+        아닐 경우 value에 0을 설정한다.
+        """
+        self.new_value = new_value
+        if 0 < self.new_value < self.limit:
+            self.value = self.new_value
+        else:
+            self.value = 0
+
     def tick(self):
-        self.second += 1
-        if self.second >= 60:
-            self.second = 0
-            self.minute += 1
-        elif self.minute >= 60:
-            self.minute = 0
-            self.hour += 1
-        elif self.hour >= 24:
-            self.hour = 0
+        """
+        value를 1 증가시킨다.
+        카운터의 값 value가 limit에 도달하면 value를 0으로 바꾼 뒤 True를 리턴한다.
+        value가 limit보다 작은 경우 False를 리턴한다.
+        """
+        self.value += 1
+        if self.value == self.limit:
+            self.value = 0
+            return True
+        elif self.value < self.limit:
+            return False
 
-    # 시계의 현재 시간 출력
     def __str__(self):
-        self.second = str(self.second).zfill(2)
-        self.minute = str(self.minute).zfill(2)
-        self.hour = str(self.hour).zfill(2)
-        return "{}:{}:{}".format(self.hour, self.minute, self.second)
+        """
+        value를 최소 두 자릿수 이상의 문자열로 리턴한다.
+        일단 str 함수로 숫자형 변수인 value를 문자열로 변환하고 zfill 메소드를 호출한다.
+        """
+        return str(self.value).zfill(2)
 
-    # 2시 3분 58초로 시계 세팅
-    def set(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
 
-# 1시 30분 48초인 시계 인스턴스 생성
-clock = Clock(1, 30, 48)
+# 최대 30까지 셀 수 있는 카운터 인스턴스 생성
+counter = Counter(30)
 
-# 13초를 늘린다
-for i in range(13):
-    clock.tick()
-
-# 시계의 현재 시간 출력
-print(clock)
-
-# 2시 3분 58초로 시계 세팅
-clock.set(2, 3, 58)
-
-# 5초를 늘린다
+# 0부터 5까지 센다
+print("1부터 5까지 카운트하기")
 for i in range(5):
-    clock.tick()
+    counter.tick()
+    print(counter)
 
-# 시계의 현재 시간 출력
-print(clock)
+# 타이머 값을 0으로 바꾼다
+print("카운터 값 0으로 설정하기")
+counter.set(0)
+print(counter)
 
-# 23시 59분 57초로 세팅
-clock.set(23, 59, 57)
+# 카운터 값 27로 설정
+print("카운터 값 27로 설정하기")
+counter.set(27)
+print(counter)
 
-# 5초를 늘린다
+# 카운터 값이 30이 되면 0으로 바뀌는지 확인
+print("카운터 값이 30이 되면 0으로 바뀝니다")
 for i in range(5):
-    clock.tick()
-
-# 시계의 현재 시간 출력
-print(clock)
+    counter.tick()
+    print(counter)
